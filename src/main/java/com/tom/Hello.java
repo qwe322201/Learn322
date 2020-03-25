f1f7442658a4e6181f0c287e64dfca0f7ba2ffb8
qwe322201 <62647828+qwe322201@users.noreply.github.com>
2020-03-25 21:48:24 +0800

    JAVA and Kotlin 練習

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -4,6 +4,16 @@ import java.sql.SQLOutput;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Heloo");
+        //System.out.println("Hello");
+     Person p=new Person();
+     p.hello();
+     int age=19;
+     char c='我';
+     byte b=120;
+     float weight =66.5f;
+     double heighrt=1.7;
+     boolean adult=true;
+     String name="tom";
+
     }
 }
