89d497d1ace11bf33a64bf61bb27378db42bc990
qwe322201 <62647828+qwe322201@users.noreply.github.com>
2020-03-27 13:44:22 +0800

    JAVA and Kotli 練習 建構子與方法 數值大小

--- src/main/java/com/tom/Hello.java
+++ src/main/java/com/tom/Hello.java
@@ -5,11 +5,15 @@ import java.sql.SQLOutput;
 public class Hello {
     public static void main(String[] args) {
         //System.out.println("Hello");
-     Person p=new Person();
+     Person p=new Person( "tom",66.5f, 1.7f);
      p.hello();
      p.weight=66.5f;
      p.height=1.7f;
      System.out.println(p.bmi());
+     int score =88;
+     System.out.println(score<60||score>90);
+     char c='A';
+      System.out.println(c> 'a');
      /*int age=19;
      char c='x';
      Character c2='A';
--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -2,15 +2,23 @@ package com.tom
 
 fun main() {
     //println("Hello")
-    val h = Human(66.5f,1.7f)
+    val h = Human(weight = 66.5f,height = 1.7f)
     println(h.bmi())
+    val score=88
+    println(score>60)
+    val c:Char='A'
+    println(c.toInt()>60)
     /*var age = 19
     age = 20
     var weight = 66.5f
     var name: String;
     name = "cc"*/
 }
-class Human(var weight: Float,var height:Float) {
+class Human(var name:String="",var weight: Float,var height:Float) {
+    init{
+        println("test $weight")
+    }
+    //constructor(name:String,weight: Float,height: Float)
     fun bmi():Float{
     val bmi =weight/(height*height)
         return bmi}
