fc1f3b0e8e7069798c8f78bcaec1cf5563c2a1bc
qwe322201 <62647828+qwe322201@users.noreply.github.com>
2020-04-01 16:21:44 +0800

    JAVA and Kotli 練習 集合 數列 以及停車時間計費使用

---
+++ src/main/java/com/parking/Car.java
@@ -0,0 +1,27 @@
+package com.parking;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Car {
+    private String id;
+    private LocalDateTime enter;
+    private LocalDateTime leave;
+    public  Car(String id, LocalDateTime enter) {
+        this.id=id;
+        this.enter =enter;
+    }
+
+    public  long getDuration(){
+        Duration duration= Duration.between(enter,leave);
+        return duration.toMinutes(); //enter.leave parking time
+    }
+    public  void leave(){
+        leave=LocalDateTime.now();
+    }
+    public void setleave(LocalDateTime leave){
+        if(leave.isAfter(enter)){
+            this.leave=leave;
+        }
+    }
+}
---
+++ src/main/kotlin/com/tom/Parking.kt
@@ -0,0 +1,25 @@
+package com.tom
+
+import java.time.Duration
+import java.time.LocalDateTime
+
+fun main() {
+    val enter=LocalDateTime.of(2018,12,25,8,0,0)
+    val leave=LocalDateTime.of(2018,12,25,10,0,0)
+    var car=Car("AAA-0001",enter)
+    car.leave=leave
+    println(car.duration())
+    val hours=Math.ceil(car.duration()/60.0).toLong()
+    println(hours)
+    println(30*hours)
+}
+    class Car(val id:String,val enter:LocalDateTime) {
+        var leave: LocalDateTime? = null
+            set(value) {
+                if (enter.isBefore (value))
+                    field = value
+            }
+    fun duration()=Duration.between(enter,leave).toMinutes()
+
+
+    }
